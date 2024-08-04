abc0dff0fdfdcbd19d4950c836ad91ac72468bfb
yizhidahaozi <167291691+yizhidahaozi@users.noreply.github.com>
2024-08-04 21:13:18 +0800

    Update aliyun2.py

--- monoscript/aliyun2.py
+++ monoscript/aliyun2.py
@@ -1,6 +1,6 @@
 import json
 import  requests
-# const $ = new Env('阿里云盘签到');
+# const $ = new Env('阿里云盘签');
 # refresh_token是一成不变的呢，我们使用它来更新签到需要的access_token，
 # refresh_token获取教程：https://github.com/bighammer-link/Common-scripts/wiki/%E9%98%BF%E9%87%8C%E4%BA%91%E7%9B%98refresh_token%E8%8E%B7%E5%8F%96%E6%96%B9%E6%B3%95
 refresh_token = ''
